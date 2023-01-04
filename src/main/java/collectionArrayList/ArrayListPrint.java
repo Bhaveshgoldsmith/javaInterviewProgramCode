5ba43a0297168da8e499bfdafee97df9e43a4e3e
bhavesh.soni <bhavesh.soni@1rstwap.com>
2023-01-04 17:19:17 +0400

    Added a program to print arraylist using different method

--- src/main/java/collectionArrayList/ArrayListPrint.java
+++ src/main/java/collectionArrayList/ArrayListPrint.java
@@ -1,6 +1,7 @@
 package collectionArrayList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListPrint {
 
@@ -13,6 +14,7 @@ public class ArrayListPrint {
 		arr.add(20);
 
 		// You can use the for loop to simply print all elements of ArrayList
+		System.out.println("****Using for loop****");
 		for (int i = 0; i < arr.size(); i++) {
 
 			System.out.println(arr.get(i));
@@ -20,7 +22,19 @@ public class ArrayListPrint {
 
 		// The println command can be used directly in the case of ArrayLists made using
 		// primitive data types.
+		System.out.println("****Print Directly when primitive data type used****");
 		System.out.println(arr);
+
+		// Print using Iterator
+		System.out.println("****Print using Iterator****");
+		Iterator it = arr.iterator();
+
+		
+		while (it.hasNext()) {
+			
+			
+			System.out.println(it.next());
+		}
 
 	}
 
