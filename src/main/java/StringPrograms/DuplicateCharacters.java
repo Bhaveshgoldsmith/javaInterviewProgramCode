25aa59b3c5261d59374938699a0f159a64ea2653
shriya.paudyal <shriya.paudyal@dxb-1rstwap-07.dxb1rstwap.com>
2023-01-08 17:58:14 +0400

    Added program for joint string and duplicate characters

---
+++ src/main/java/StringPrograms/DuplicateCharacters.java
@@ -0,0 +1,17 @@
+package StringPrograms;
+public class DuplicateCharacters {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "My Name is Bhavesh";
+		char c[] = str.toCharArray();
+		System.out.print("Duplicate characters are: ");
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j < str.length(); j++) {
+				if (c[i] == c[j]) {
+					System.out.print(c[j] + " ");
+					break;
+				}
+			}
+		}
+	}
+}
