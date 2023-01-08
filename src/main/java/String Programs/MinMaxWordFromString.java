d3dd392002d51aa4151792c99ee9da197d4cd5ae
shriya.paudyal <shriya.paudyal@dxb-1rstwap-07.dxb1rstwap.com>
2023-01-08 12:32:40 +0400

    Added string programs

---
+++ src/main/java/String Programs/MinMaxWordFromString.java
@@ -0,0 +1,25 @@
+package JavaPrograms;
+public class MinMaxWordFromString {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "My Name is a Bhavesh";
+		String[] arr = str.split(" ");
+		int i = 0;
+		int maxlength, minlength;
+		maxlength = Integer.MIN_VALUE;
+		minlength = Integer.MAX_VALUE;
+		String largest, smallest;
+		largest = smallest = "";
+		for (i = 0; i < arr.length; i++) {
+			if (arr[i].length() < minlength) {
+				smallest = arr[i];
+				minlength = arr[i].length();
+			}
+			if (arr[i].length() > maxlength) {
+				largest = arr[i];
+				maxlength = arr[i].length();
+			}
+		}
+		System.out.println("The largest and smallest word is \"" + largest + "\" and \"" + smallest + "\"");
+	}
+}
---
+++ src/main/java/String Programs/ReverseString.java
@@ -0,0 +1,16 @@
+package JavaPrograms;
+public class ReverseString {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Madamt";
+		String a = " ";
+		System.out.println("Original String");
+		for (int i = 0; i < s.length(); i++) {
+			System.out.print(s.charAt(i) + " ");
+		}
+		System.out.println("");
+		for (int i = s.length() - 1; i >= 0; i--) {
+			System.out.print(s.charAt(i) + " ");
+		}
+	}
+}
