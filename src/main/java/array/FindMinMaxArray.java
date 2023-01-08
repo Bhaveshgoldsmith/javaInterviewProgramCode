329d5bece965d38bb19aafd35e821fa551b3460f
shriya.paudyal <shriya.paudyal@dxb-1rstwap-07.dxb1rstwap.com>
2023-01-08 10:07:07 +0400

    added min/max array, reverse and swap array

---
+++ src/main/java/array/FindMinMaxArray.java
@@ -0,0 +1,17 @@
+package JavaPrograms;
+public class FindMinMaxArray {
+	public static void main(String[] args) {
+		int num[] = { 13, 6, 15, 4, 18, 9 };
+		int min = num[0];
+		int max = num[0];
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] < min) {
+				min = num[i];
+			} else if (num[i] > max) {
+				max = num[i];
+			}
+		}
+		System.out.println("The minimum array element is " + min);
+		System.out.println("The maximum array element is " + max);
+	}
+}
---
+++ src/main/java/array/MinMaxTwoDimensionArray.java
@@ -0,0 +1,35 @@
+package JavaPrograms;
+public class MinMaxTwoDimensionArray {
+//	
+//	11 12 13
+//	21 22 22
+//	0  32 33
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][] = { { 11, 12, 13 }, { 21, 22, 23 }, { 33, 32, 0 } };
+		int min = arr[0][0];
+		int max = arr[0][0];
+		int minColumn = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (arr[i][j] < min) {
+					min = arr[i][j];
+					minColumn=j;
+				} else if (arr[i][j] > max) {
+					max = arr[i][j];
+				}
+			}
+		}
+		System.out.println("Minimum value within array is: " + min + "\n" + "Maximum value within array is: " + max);
+		int k = 0;
+		int maxColumn = arr[k][minColumn];
+		for(k=0;k<3;k++)
+		{
+			if(arr[k][minColumn]>maxColumn)
+			{
+				maxColumn=arr[k][minColumn];
+			}
+		}
+		System.out.println("Maximum value in column is: " + maxColumn);
+	}
+}
